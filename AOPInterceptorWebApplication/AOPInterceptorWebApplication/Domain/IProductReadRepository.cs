c7bb3a15592813fe418d7d2e204bbb2757f121e5
f_nosrati <f_nosrati@SHL-62>
2023-05-23 20:46:33 +0330

    Add project

---
+++ AOPInterceptorWebApplication/AOPInterceptorWebApplication/Domain/IProductReadRepository.cs
@@ -0,0 +1,13 @@
+﻿using AOPInterceptorWebApplication.Domain.Entitys;
+
+namespace AOPInterceptorWebApplication.Domain
+{
+    public interface IProductReadRepository
+    {
+        Task<Product> GetProductAsync(int productId);
+
+        Task<IEnumerable<Product>> GetProductsAsync();
+
+        Task<bool> IsExistProductAsync(int productId);
+    }
+}
---
+++ AOPInterceptorWebApplication/AOPInterceptorWebApplication/Services/Contracts/IProductServices.cs
@@ -0,0 +1,18 @@
+﻿using AOPInterceptorWebApplication.InputModels.ProductInputModels;
+using AOPInterceptorWebApplication.ViewModels.ProductViewModels;
+
+namespace AOPInterceptorWebApplication.Services.Contracts
+{
+    public interface IProductServices
+    {
+        Task<int> CreateProductAsync(CreateProductInputModel inputModel);
+
+        Task UpdateProductAsync(UpdateProductInputModel inputModel);
+
+        Task DeleteProductAsync(int productId);
+
+        ValueTask<ProductViewModel> GetProductAsync(int productId);
+
+        ValueTask<IEnumerable<ProductViewModel>> GetProductsAsync();
+    }
+}
